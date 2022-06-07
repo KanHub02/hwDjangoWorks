1e7a9d3a97de6a7556086d9f7b3277542cd151ac
Usmanov Kanat <usmnvk200207@gmail.com>
2022-06-07 16:49:54 +0600

    Hw8

--- book/tests.py
+++ book/tests.py
@@ -1,3 +1,87 @@
 from django.test import TestCase
+from . import models
+from datetime import date
 
-# Create your tests here.
+
+class TestModel(TestCase):
+    def test_model_shows_create_success(self):
+        payload = {
+            'title': "Test title",
+            'author': 'Test author',
+            'description': 'Test description',
+            'image': 'Test image',
+            'genre': 'Test Genre',
+            'quantity': 5,
+            'created_date': date.today(),
+            'updated_date': date.today(),
+            'age_control': 18,
+
+        }
+        post = models.Book.objects.create(**payload)
+        self.assertEqual(post.title, payload['title'])
+        self.assertEqual(post.author, payload['author'])
+        self.assertEqual(post.description, payload['description'])
+        self.assertEqual(post.image, payload['image'])
+        self.assertEqual(post.genre, payload['genre'])
+        self.assertEqual(post.quantity, payload['quantity'])
+        self.assertEqual(post.created_date, payload['created_date'])
+        self.assertEqual(post.updated_date, payload['updated_date'])
+        self.assertEqual(post.age_control, payload['age_control'])
+
+    def test_model_shows_create_fail(self):
+        payload = {
+            'title': "Test title",
+            'author': 'Test author',
+            'description': 'Test description',
+            'image': 'Test image',
+            'genre': 'Test Genre',
+            'quantity': 5,
+            'created_date': date.today(),
+            'updated_date': date.today(),
+            'age_control': 'negr',
+
+        }
+
+        with self.assertRaises(ValueError):
+            post = models.Book.objects.create(**payload)
+
+        pass
+
+    def test_model_shows_update(self):
+        payload = {
+            'title': "Test title",
+            'author': 'Test author',
+            'description': 'Test description',
+            'image': 'Test image',
+            'genre': 'Test Genre',
+            'quantity': 5,
+            'created_date': date.today(),
+            'updated_date': date.today(),
+            'age_control': 18,
+
+        }
+        newtitle = 'New Title'
+        post = models.Book.objects.create(**payload)
+        post.title = newtitle
+        post.save()
+        post.refresh_from_db()
+        self.assertEqual(post.title, newtitle)
+
+    def test_model_shows_delete(self):
+        payload = {
+            'title': "Test title",
+            'author': 'Test author',
+            'description': 'Test description',
+            'image': 'Test image',
+            'genre': 'Test Genre',
+            'quantity': 5,
+            'created_date': date.today(),
+            'updated_date': date.today(),
+            'age_control': 18,
+
+        }
+        post = models.Book.objects.create(**payload)
+        pk = post.pk
+        post.delete()
+        with self.assertRaises(models.Book.DoesNotExist):
+            models.Book.objects.get(pk=pk)
--- main/settings.py
+++ main/settings.py
@@ -33,8 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'book',
-    'author',
-    'blog'
+    'blog',
 ]
 
 MIDDLEWARE = [
